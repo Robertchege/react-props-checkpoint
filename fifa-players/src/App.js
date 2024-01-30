dc83b4f9e98c3e4a249d123b5872817c8a1ad9ef
Robert Chege <rchege737@gmail.com>
2024-01-30 21:32:59 +0300

    first update

---
+++ fifa-players/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Import the PlayerList component
+import PlayerList from "./PlayerList";
+
+// Functional component for the main App
+export default function App() {
+  return (
+    <div className="App">
+      {/* Heading for the application */}
+      <h1>Football Players</h1>
+
+      {/* Render the PlayerList component */}
+      <PlayerList />
+    </div>
+  );
+}
---
+++ fifa-players/src/players.js
@@ -0,0 +1,38 @@
+// Array containing player data
+const playersData = [
+    {
+      name: "chelsea",
+      team: "chelsea",
+      nationality: "Swiss",
+      jerseyNumber: 1,
+      age: 28,
+      imageUrl: "pics/chelsea.jpg",
+    },
+    {
+      name: "diego",
+      team: "Man u",
+      nationality: "french",
+      jerseyNumber: 13,
+      age: 20,
+      imageUrl: "pics/diego.jpg",
+    },
+    {
+      name: "fred",
+      team: "Juventus FC",
+      nationality: "French",
+      jerseyNumber: 11,
+      age: 20,
+      imageUrl: "pics/fred.jpg",
+    },
+    {
+      name: "haaland",
+      team: "arsenal",
+      nationality: "spain",
+      jerseyNumber: 5,
+      age: 23,
+      imageUrl: "pics/haaland.jpg",
+    },
+  ];
+  
+  // Export the array for external use
+  export default playersData
